12cba035e6b76f19c3208855c89b6616f8aaea2a
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2021-03-27 18:11:35 -0500

    Storybook Setup (#2)
    * storybook setup WIP
    
    * uncomment user type
    
    * basic setup and meme outline
    
    * remove unused story
    
    Co-authored-by: Adam Sheaffer <adamsheaffer@Adams-MacBook-Pro-2.local>

---
+++ client/src/components/Meme.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <h1>{{ top }}</h1>
+    <div>
+      <img :src="imageURL" alt="">
+    </div>
+    <h1>{{ bottom }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Meme extends Vue {
+  @Prop() private imageURL!: string
+
+  @Prop() private top!: string
+
+  @Prop() private bottom!: string
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/stories/Meme.stories.js
@@ -0,0 +1,28 @@
+import Meme from '../components/Meme.vue';
+
+export default {
+  title: 'Memer/Meme',
+  component: Meme,
+  argTypes: {
+    imageURL: {
+      control: { type: 'text' },
+      defaultValue: 'https://media.giphy.com/media/AcfTF7tyikWyroP0x7/giphy.gif',
+    },
+    top: {
+      control: { type: 'text' },
+      defaultValue: 'Hello',
+    },
+    bottom: {
+      control: { type: 'text' },
+      defaultValue: 'Hello',
+    },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Meme },
+  template: '<meme v-bind="$props" />',
+});
+
+export const Basic = Template.bind({});
