0cbb94e0a2d8236db40c536b0256294d109e7c32
Adam Sheaffer <adamsheaffer@Adams-MacBook-Pro-2.local>
2021-04-19 10:53:05 -0500

    initial player hand component

---
+++ client/src/components/PlayerHand.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="player-hand">
+    <div v-for="card in cards" :key="card.id" class="player-card">
+      <section class="text top">
+        <p>{{ card.top }}</p>
+      </section>
+      <section class="text bottom">
+        <p>{{ card.bottom }}</p>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from 'vue-property-decorator';
+import { Card } from '@/types/Card';
+@Component
+export default class PlayerChip extends Vue {
+  @Prop({ required: true }) cards!: Card[]
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/_variables';
+.player-hand {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  grid-gap: .5rem;
+}
+.player-card {
+  @include impact-text;
+  text-shadow: none;
+  border: 2px solid $primary;
+  background: $purple-2;
+  font-size: .8rem;
+  display: grid;
+  align-content: space-between;
+  height: 200px;
+}
+.text {
+  text-align: center;
+  padding: 1rem;
+}
+</style>
