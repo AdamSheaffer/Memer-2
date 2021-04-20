5e9f1d4ca3047946952f3fe6b65861961c9b1fc3
Nate V <navogel@gmail.com>
2021-04-19 20:27:12 -0500

    added story and component - copied from player chip

---
+++ client/src/stories/Button.stories.js
@@ -0,0 +1,42 @@
+import PlayerChip from '../components/PlayerChip.vue';
+
+export default {
+  title: 'Memer/PlayerChip',
+  component: PlayerChip,
+  argTypes: {
+    player: {
+      control: { type: 'object' },
+      defaultValue: {
+        uid: 'abc123',
+        fullName: 'Jeff Lebowski',
+        username: 'The Dude',
+        photoURL: 'https://bamfstyle.com/wp-content/uploads/2016/04/dude6-main.jpg',
+        isActive: false,
+        score: 3,
+        roles: { player: true },
+      },
+    },
+    pointsToWin: {
+      control: { type: 'range', min: 1, max: 10 },
+      defaultValue: 7,
+    },
+    active: {
+      control: { type: 'boolean' },
+      defaultValue: false,
+    },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { PlayerChip },
+  template: '<player-chip v-bind="$props" />',
+});
+
+export const Inactive = Template.bind({});
+
+export const Active = Template.bind({});
+Active.args = {
+  ...Inactive.args,
+  active: true,
+};
