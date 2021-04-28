5e9fa2948d6008dd9762ab31ed183153c55b5ae7
Adam Sheaffer <adamsheaffer@Adams-MacBook-Pro-2.local>
2021-04-28 17:39:45 -0500

    add function for seed data

---
+++ functions/src/data/data.ts
@@ -0,0 +1,145 @@
+export default {
+  categories: [
+    {description: "mirin"},
+    {description: "michael scott"},
+    {description: "kanye west"},
+    {description: "meme"},
+    {description: "mrw"},
+    {description: "the office"},
+    {description: "alcohol"},
+    {description: "chuck norris"},
+    {description: "gif soup"},
+    {description: "gross"},
+    {description: "delicious"},
+    {description: "beyonce"},
+    {description: "fail"},
+    {description: "james franco"},
+  ],
+  captions: [
+    {
+      top: null,
+      bottom: "duct tape should do it",
+      safeForWork: true,
+    },
+    {
+      top: null,
+      bottom: "big if true",
+      safeForWork: true,
+    },
+    {
+      top: "your medal",
+      bottom: "is in the mail",
+      safeForWork: true,
+    },
+    {
+      top: "My NSA agent",
+      bottom: "when i switch to incognito mode",
+      safeForWork: true,
+    },
+    {
+      top: "when the teacher",
+      bottom: "asks for a volunteer",
+      safeForWork: true,
+    },
+    {
+      top: null,
+      bottom: "fall of the berlin wall (1989)",
+      safeForWork: true,
+    },
+    {
+      top: null,
+      bottom: "she dooooo",
+      safeForWork: true,
+    },
+    {
+      top: "when you suddenly remember",
+      bottom: "what you did in a blackout",
+      safeForWork: true,
+    },
+    {
+      top: null,
+      bottom: "as seen on t.v.",
+      safeForWork: true,
+    },
+    {
+      top: null,
+      bottom: "well they seem nice",
+      safeForWork: true,
+    },
+    {
+      top: null,
+      bottom: "that can't be sanitary",
+      safeForWork: true,
+    },
+    {
+      top: null,
+      bottom: "diet starts tomorrow",
+      safeForWork: true,
+    },
+    {
+      top: null,
+      bottom: "trust me I'm a doctor",
+      safeForWork: true,
+    },
+    {
+      top: null,
+      bottom: "::confusion intensifies::",
+      safeForWork: true,
+    },
+    {
+      top: null,
+      bottom: "dope etsy store",
+      safeForWork: true,
+    },
+    {
+      top: null,
+      bottom: "how neckbeards think they look",
+      safeForWork: true,
+    },
+    {
+      top: "when you hold the bag",
+      bottom: "and eat everyone's fries",
+      safeForWork: true,
+    },
+    {
+      top: "when you're trick-or-treating",
+      bottom: "and someone gives you raisins",
+      safeForWork: true,
+    },
+    {
+      top: "a novel from",
+      bottom: "stephen king",
+      safeForWork: true,
+    },
+    {
+      top: null,
+      bottom: "is this k-pop?",
+      safeForWork: true,
+    },
+    {
+      top: "welcome",
+      bottom: "to the thunder dome",
+      safeForWork: true,
+    },
+    {
+      top: "knew this art degree",
+      bottom: "would come in handy",
+      safeForWork: true,
+    },
+    {
+      top: "me:",
+      bottom: "netflix: are you still there?",
+      safeForWork: true,
+    },
+    {
+      top: null,
+      bottom: "is self doubt an instrument?",
+      safeForWork: true,
+    },
+    {
+      top: "who's that",
+      bottom: "pokemon?",
+      safeForWork: true,
+    },
+  ],
+};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,3 +7,4 @@ export * from "./game/gameFull";
 export * from "./players/registerGamePlayed";
 export * from "./presence/updateOnlineCounter";
 export * from "./presence/userStatusChanged";
+export * from "./data/emulatorPopulate";
