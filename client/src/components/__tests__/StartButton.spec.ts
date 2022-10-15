546328a6031024937286488300de7b9fda3260ce
Adam Sheaffer <asheaffer@appdat.jsc.nasa.gov>
2022-10-15 14:06:55 -0500

    update start button tests

--- client/src/components/__tests__/StartButton.spec.ts
+++ client/src/components/__tests__/StartButton.spec.ts
@@ -1,44 +1,106 @@
-import { mount } from "@vue/test-utils";
+import { mount, RouterLinkStub } from "@vue/test-utils";
 import { afterEach, describe, expect, it, vi } from "vitest";
 import { ref } from "vue";
-import { Player } from "../../../../types";
-import StartButton from "../PreGameDisplay.vue";
+import { Player, User } from "../../../../types";
+import PreGameDisplay from "../PreGameDisplay.vue";
 
+// useUser Mocks
+const mockUser = ref<Partial<User>>({ uid: "abc" });
+
+// useGame Mocks
 const mockStartGameFn = vi.fn();
+const mockPlayers = ref<Partial<Player>[]>([]);
 const mockActivePlayers = ref<Partial<Player>[]>([]);
+const mockUserIsHost = ref(false);
+const mockHost = ref<Partial<Player>>({ uid: "1", username: "The Dude" });
 
-describe("StartButton", () => {
-  vi.mock("../../composeables/useGame", () => ({
+// useDeck Mocks
+const mockDealToPlayers = vi.fn();
+
+describe("PreGameDisplay", () => {
+  vi.mock("../../composables/useGame", () => ({
     useGame: () => ({
       activePlayers: mockActivePlayers,
+      players: mockPlayers,
       startGame: mockStartGameFn,
+      userIsHost: mockUserIsHost,
+      host: mockHost,
+    }),
+  }));
+  vi.mock("../../composables/useUser", () => ({
+    useUser: () => ({
+      user: mockUser,
+    }),
+  }));
+  vi.mock("../../composables/useDeck", () => ({
+    useDeck: () => ({
+      dealToPlayers: mockDealToPlayers,
     }),
   }));
 
   afterEach(() => {
+    mockPlayers.value = [];
     mockActivePlayers.value = [];
+    mockUserIsHost.value = false;
+    mockHost.value = { uid: "1", username: "The Dude" };
     vi.clearAllMocks();
   });
 
-  it("disables button and shows message if waiting on players", async () => {
-    const wrapper = mount(StartButton, {
-      props: { gameId: `sample_game_id` },
+  it("hides start button for non-host players and shows waiting on players message", () => {
+    mockUserIsHost.value = false;
+    mockActivePlayers.value = [{ uid: "1" }, { uid: "2" }];
+    const wrapper = mount(PreGameDisplay, {
+      props: { gameId: "sample_game_id" },
+      stubs: { RouterLink: RouterLinkStub },
+    });
+
+    const startBtn = wrapper.find("[data-start-button]");
+    const msg = wrapper.find("[data-waiting-on-players]");
+
+    expect(startBtn.exists()).toBe(false);
+    expect(msg.text()).toBe("WAITING ON MORE PLAYERS...");
+  });
+
+  it("hides start button for non-host players and shows waiting on host message with minimum players", () => {
+    mockHost.value = { uid: "1", username: "THE DUDE" };
+    mockUserIsHost.value = false;
+    mockActivePlayers.value = [{ uid: "1" }, { uid: "2" }, { uid: "3" }];
+    const wrapper = mount(PreGameDisplay, {
+      props: { gameId: "sample_game_id" },
+      stubs: { RouterLink: RouterLinkStub },
+    });
+
+    const startBtn = wrapper.find("[data-start-button]");
+    const msg = wrapper.find("[data-waiting-on-host]");
+
+    expect(startBtn.exists()).toBe(false);
+    expect(msg.text()).toBe("WAITING ON THE DUDE TO START THE GAME...");
+  });
+
+  it("disables button and shows message if host is waiting on players", async () => {
+    mockUserIsHost.value = true;
+    const wrapper = mount(PreGameDisplay, {
+      props: { gameId: "sample_game_id" },
+      stubs: { RouterLink: RouterLinkStub },
     });
 
     const msg = wrapper.find("[data-waiting-on-players]");
-    const btn = wrapper.find("button");
+    const btn = wrapper.find("[data-start-button]");
     await btn.trigger("click");
 
     expect(msg.exists()).toBe(true);
-    expect(msg.text()).toBe("Waiting on more players");
+    expect(msg.text()).toBe("WAITING ON MORE PLAYERS...");
     expect(btn.attributes().disabled).toBe("true");
     expect(mockStartGameFn).not.toHaveBeenCalled();
   });
 
-  it("enables button and calls start game when clicked", async () => {
+  it("enables button for host and calls start game when clicked", async () => {
+    mockUserIsHost.value = true;
     mockActivePlayers.value = [{ uid: "1" }, { uid: "2" }, { uid: "3" }];
-    const wrapper = mount(StartButton, {
-      props: { gameId: `sample_game_id` },
+
+    const wrapper = mount(PreGameDisplay, {
+      props: { gameId: "sample_game_id" },
+      stubs: { RouterLink: RouterLinkStub },
     });
 
     const msg = wrapper.find("[data-waiting-on-players]");
