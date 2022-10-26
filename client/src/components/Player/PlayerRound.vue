1c4085eebe82bedd830a86888633e40c4ffe6740
Adam Sheaffer <asheaffer@appdat.jsc.nasa.gov>
2022-10-25 22:03:09 -0500

    switch giphy carousel to async component

--- client/src/components/Player/PlayerRound.vue
+++ client/src/components/Player/PlayerRound.vue
@@ -1,14 +1,16 @@
 <script lang="ts" setup>
+import { defineAsyncComponent } from "vue";
 import { useGame } from "../../composables/useGame";
 import GameWinner from "../GameWinner.vue";
 import RoundWinner from "../RoundWinner.vue";
 import SubmitCaption from "./SubmitCaption.vue";
-import SubmitGif from "./SubmitGif.vue";
 import WaitingForCategorySelection from "./WaitingForCategorySelection.vue";
 import WaitingForGifSelection from "./WaitingForGifSelection.vue";
 import WaitingForReverseRoundCaption from "./WaitingForReverseRoundCaption.vue";
 import WaitingForTurnStart from "./WaitingForTurnStart.vue";
 import WaitingForWinnerSelection from "./WaitingForWinnerSelection.vue";
+
+const SubmitGif = defineAsyncComponent(() => import("./SubmitGif.vue"));
 
 const props = defineProps<{ gameId: string }>();
 
