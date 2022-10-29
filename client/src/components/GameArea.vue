38424612375da043fea56cb171e7c58dbe529616
Adam Sheaffer <asheaffer@appdat.jsc.nasa.gov>
2022-10-29 15:55:31 -0500

    fix import path

--- client/src/components/GameArea.vue
+++ client/src/components/GameArea.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useGame } from "../composables/useGame";
-import JudgeRound from "./Judge_temp/JudgeRound.vue";
+import JudgeRound from "./Judge/JudgeRound.vue";
 import PlayerRound from "./Player/PlayerRound.vue";
 import PreGameDisplay from "./PreGameDisplay.vue";
 
